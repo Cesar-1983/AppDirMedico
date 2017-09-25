f0e3bd08c974ecf63268029bf8e21a9417aeed26
César Augusto Rivera Rivas <crivera@versatec.biz>
2017-09-25 17:30:59 -0600

    Primer Commit

---
+++ AppIdentity.Samples/AppIdentity.Samples/Models/IdentityModels.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace IdentitySample.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        static ApplicationDbContext()
+        {
+            // Set the database intializer which is run once during application start
+            // This seeds the database with admin user credentials and admin role
+            Database.SetInitializer<ApplicationDbContext>(new ApplicationDbInitializer());
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+
+    public class TipoEspecialidad {
+        public int TipoEspecialidadID { get; set; }
+        public string Descripcion { get; set; }
+
+    }
+
+    public class Especialidad {
+        public int EspecialidadID { get; set; }
+        public string Descripcion { get; set; }
+
+        public virtual TipoEspecialidad TipoEspecialidad { get; set; }
+
+        public virtual PerfilMedico PerfilMedico { get; set; }
+    }
+
+    public class PerfilMedico {
+        public int PerfilMedicoID { get; set; }
+
+        [StringLength(100)]
+        public string PrimerNombre { get; set; }
+
+        [StringLength(100)]
+        public string SegundoNombre { get; set; }
+
+        [StringLength(100)]
+        public string PrimerApellido { get; set; }
+
+        [StringLength(100)]
+        public string SegundoApellido { get; set; }
+
+        [StringLength(300)]
+        public string Descripcion { get; set; }
+
+        public byte[] Photo { get; set; }
+        public virtual Especialidad Especialidad { get; set; }
+    }
+}
